e30562afcc7be9be207b3c50fbb81a754310af0f
EnriqueMoranGarrido <enrique.morango@udlap.mx>
2022-09-29 16:53:30 -0500

    created list of sales person and iterate through it in the html template

--- src/app/sales-person-list/sales-person-list.component.ts
+++ src/app/sales-person-list/sales-person-list.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
   templateUrl: './sales-person-list.component.html',
-  styleUrls: ['./sales-person-list.component.css']
+  styleUrls: ['./sales-person-list.component.css'],
 })
 export class SalesPersonListComponent implements OnInit {
+  //create an array of sales person objects
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson('Morgan', 'Moran', 'mora@mail.com', 1),
+    new SalesPerson('Iris', 'Moran', 'iris@mail.com', 2),
+    new SalesPerson('Eve', 'Moran', 'eve@mail.com', 3),
+    new SalesPerson('Matt', 'Moran', 'matt@mail.com', 4),
+    new SalesPerson('Max', 'Moran', 'max@mail.com', 5),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
